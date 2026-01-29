f5b3409e47984f0c5cb430d1a2cb17989378e786
Ashraful Islam <youremail@example.com>
2026-01-29 15:19:14 +0600

    Added those extra method without post and put all working

--- EchoV4/handlers/userHandler.go
+++ EchoV4/handlers/userHandler.go
@@ -18,10 +18,6 @@ func CreateUser(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, response.ErrorResponse("Invalid request", err.Error()))
 	}
 
-	if err := c.Validate(&req); err != nil {
-		return c.JSON(http.StatusBadRequest, response.ErrorResponse("Validation Failed", err.Error()))
-	}
-
 	user := request.User{
 		Id:    strconv.FormatInt(time.Now().Unix(), 10),
 		Name:  req.Name,
@@ -30,4 +26,66 @@ func CreateUser(c echo.Context) error {
 	}
 
 	return c.JSON(201, response.SuccessResponse("User created successfully", user))
+}
+
+// GetUser Single user - GET /users/:id
+func GetUser(c echo.Context) error {
+	id := c.Param("id")
+
+	user := request.User{
+		Id:    id,
+		Name:  "Ashraful",
+		Email: "xyz@mail.com",
+		Age:   30,
+	}
+
+	return c.JSON(http.StatusOK, response.SuccessResponse("find user", user))
+}
+
+// UpdateUser - PUT /users/:id
+func UpdateUser(c echo.Context) error {
+	id := c.Param("id")
+
+	var req request.UpdateUserRequest
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, response.ErrorResponse("Invalid request", err.Error()))
+	}
+
+	// Update logic
+	updatedUser := request.User{
+		Id:    id,
+		Name:  req.Name,
+		Email: req.Email,
+		Age:   25, // Default age
+	}
+
+	return c.JSON(http.StatusOK, response.SuccessResponse("User updated successfully", updatedUser))
+}
+
+// Delete user
+func DeleteUser(c echo.Context) error {
+	id := c.Param("id")
+
+	return c.JSON(http.StatusOK, response.SuccessResponse("User Deleted Successfully", map[string]string{
+		"id": id}))
+}
+
+// Get All user
+func GetAllUsers(c echo.Context) error {
+	users := []request.User{
+		{
+			Id:    "1",
+			Name:  "Ashraful",
+			Email: "ash@mail.com",
+			Age:   25,
+		},
+		{
+			Id:    "2",
+			Name:  "Rahim",
+			Email: "rahim@mail.com",
+			Age:   29,
+		},
+	}
+
+	return c.JSON(http.StatusOK, response.SuccessResponse("Users Retrived Successfully", users))
 }
--- EchoV4/main.go
+++ EchoV4/main.go
@@ -5,12 +5,29 @@ import (
 
 	"EchoV4/handlers"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
+// Custom Validator
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+// Validate implements echo.Validator
+func (cv *CustomValidator) Validate(i interface{}) error {
+	if err := cv.validator.Struct(i); err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
 	e := echo.New()
+
+	// Initialize validator
+	e.Validator = &CustomValidator{validator: validator.New()}
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
@@ -19,7 +36,12 @@ func main() {
 		return c.String(http.StatusOK, "Hello, Echo V4!")
 	})
 
+	//All user route
 	e.POST("/users", handlers.CreateUser)
+	e.GET("/users/:id", handlers.GetUser)
+	e.PUT("/users/:id", handlers.UpdateUser)
+	e.DELETE("/users/:id", handlers.DeleteUser)
+	e.GET("/users", handlers.GetAllUsers)
 
 	if err := e.Start(":8080"); err != nil {
 		e.Logger.Fatal(err)
